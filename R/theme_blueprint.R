acb364c1d704491a7cbe29880d39884e38b7675b
context-dependent <tmcmanus@blueprint-ade.ca>
2019-04-08 11:30:32 -0400

    First commit, basic theme ripped off from hrbrthemes::theme_ipsum

---
+++ R/theme_blueprint.R
@@ -0,0 +1,183 @@
+theme_blueprint <- function(base_family="Segoe UI Semilight", base_size = 11.5,
+                        plot_title_family="Segoe UI Semilight", plot_title_size = 18,
+                        plot_title_face = "plain",
+                        plot_title_margin = 10,
+                        subtitle_family="Consolas", subtitle_size = 12,
+                        subtitle_face = "plain", subtitle_margin = 15,
+                        subtitle_color = "grey60",
+                        strip_text_family = base_family, strip_text_size = 12,
+                        strip_text_face = "plain",
+                        caption_family = base_family, caption_size = 9,
+                        caption_face = "italic", caption_margin = 10,
+                        axis_text_size = base_size,
+                        axis_title_family = base_family, axis_title_size = base_size,
+                        axis_title_face = "plain", axis_title_just = "rt",
+                        plot_margin = margin(30, 30, 30, 30),
+                        grid_col = "grey50", grid = TRUE,
+                        axis_col = "grey30", axis = TRUE, ticks = FALSE) {
+
+  ret <- ggplot2::theme_minimal(base_family=base_family, base_size=base_size)
+
+  ret <- ret + theme(legend.background=element_blank())
+  ret <- ret + theme(legend.key=element_blank())
+  ret <- ret + theme(legend.position = "bottom")
+
+  if (inherits(grid, "character") | grid == TRUE) {
+
+    ret <- ret + theme(panel.grid=element_line(color=grid_col, size=0.1))
+    ret <- ret + theme(panel.grid.major=element_line(color=grid_col, size=0.2))
+    ret <- ret + theme(panel.grid.minor=element_line(linetype = 2, color=grid_col, size=0.15))
+
+    if (inherits(grid, "character")) {
+      if (regexpr("X", grid)[1] < 0) ret <- ret + theme(panel.grid.major.x=element_blank())
+      if (regexpr("Y", grid)[1] < 0) ret <- ret + theme(panel.grid.major.y=element_blank())
+      if (regexpr("x", grid)[1] < 0) ret <- ret + theme(panel.grid.minor.x=element_blank())
+      if (regexpr("y", grid)[1] < 0) ret <- ret + theme(panel.grid.minor.y=element_blank())
+    }
+
+  } else {
+    ret <- ret + theme(panel.grid=element_blank())
+  }
+
+  if (inherits(axis, "character") | axis == TRUE) {
+    ret <- ret + theme(axis.line=element_line(color="#2b2b2b", size=0.15))
+    if (inherits(axis, "character")) {
+      axis <- tolower(axis)
+      if (regexpr("x", axis)[1] < 0) {
+        ret <- ret + theme(axis.line.x=element_blank())
+      } else {
+        ret <- ret + theme(axis.line.x=element_line(color=axis_col, size=0.15))
+      }
+      if (regexpr("y", axis)[1] < 0) {
+        ret <- ret + theme(axis.line.y=element_blank())
+      } else {
+        ret <- ret + theme(axis.line.y=element_line(color=axis_col, size=0.15))
+      }
+    } else {
+      ret <- ret + theme(axis.line.x=element_line(color=axis_col, size=0.15))
+      ret <- ret + theme(axis.line.y=element_line(color=axis_col, size=0.15))
+    }
+  } else {
+    ret <- ret + theme(axis.line=element_blank())
+  }
+
+  if (!ticks) {
+    ret <- ret + theme(axis.ticks = element_blank())
+    ret <- ret + theme(axis.ticks.x = element_blank())
+    ret <- ret + theme(axis.ticks.y = element_blank())
+  } else {
+    ret <- ret + theme(axis.ticks = element_line(size=0.15))
+    ret <- ret + theme(axis.ticks.x = element_line(size=0.15))
+    ret <- ret + theme(axis.ticks.y = element_line(size=0.15))
+    ret <- ret + theme(axis.ticks.length = grid::unit(5, "pt"))
+  }
+
+  xj <- switch(tolower(substr(axis_title_just, 1, 1)), b=0, l=0, m=0.5, c=0.5, r=1, t=1)
+  yj <- switch(tolower(substr(axis_title_just, 2, 2)), b=0, l=0, m=0.5, c=0.5, r=1, t=1)
+
+  ret <- ret + theme(axis.text.x=element_text(size=axis_text_size, margin=margin(t=0)))
+  ret <- ret + theme(axis.text.y=element_text(size=axis_text_size, margin=margin(r=0)))
+  ret <- ret + theme(axis.title=element_text(size=axis_title_size, family=axis_title_family))
+  ret <- ret + theme(axis.title.x=element_text(hjust=xj, size=axis_title_size,
+                                               family=axis_title_family, face=axis_title_face))
+  ret <- ret + theme(axis.title.y=element_text(hjust=yj, size=axis_title_size, angle = 90,
+                                               family=axis_title_family, face=axis_title_face))
+  ret <- ret + theme(axis.title.y.right=element_text(hjust=yj, size=axis_title_size, angle=90,
+                                                     family=axis_title_family, face=axis_title_face))
+  ret <- ret + theme(strip.text=element_text(hjust=0, size=strip_text_size,
+                                             face=strip_text_face, family=strip_text_family))
+  ret <- ret + theme(panel.spacing=grid::unit(2, "lines"))
+  ret <- ret + theme(plot.title=element_text(hjust=0, size=plot_title_size,
+                                             margin=margin(b=plot_title_margin),
+                                             family=plot_title_family, face=plot_title_face))
+  ret <- ret + theme(plot.subtitle=element_text(hjust=0, size=subtitle_size,
+                                                margin=margin(b=subtitle_margin),
+                                                family=subtitle_family, face=subtitle_face,
+                                                color = subtitle_color))
+  ret <- ret + theme(plot.caption=element_text(hjust=1, size=caption_size,
+                                               margin=margin(t=caption_margin),
+                                               family=caption_family, face=caption_face))
+  ret <- ret + theme(plot.margin=plot_margin)
+
+  ret
+
+}
+
+#' Update matching font defaults for text geoms
+#'
+#' Updates [ggplot2::geom_label] and [ggplot2::geom_text] font defaults
+#'
+#' @param family,face,size,color font family name, face, size and color
+#' @export
+update_geom_font_defaults <- function(family="Segoe UI Semilight", face="plain", size=3.5,
+                                      color = "#2b2b2b") {
+  update_geom_defaults("text", list(family=family, face=face, size=size, color=color))
+  update_geom_defaults("label", list(family=family, face=face, size=size, color=color))
+}
+
+#' @rdname ArialNarrow
+#' @md
+#' @title Segoe UI Semilight font name R variable aliases
+#' @description `font_an` == "`Segoe UI Semilight`"
+#' @format length 1 character vector
+#' @export
+font_an <- "Segoe UI Semilight"
+
+
+scale_fill_blueprint <- function(discrete = FALSE, ...) {
+
+  if(!discrete) {
+    res <- viridis::scale_fill_viridis(option = "cividis", ...)
+  } else {
+
+    discrete_palette <- c(
+      "#FFEB82",
+      "#D2E6F5",
+      "#DCE682",
+      "#FABE78",
+      "#9BB9DC",
+      "#AFBE82",
+      "#DC966E"
+    )
+
+    res <- scale_fill_manual(values = discrete_palette)
+  }
+
+  res
+}
+
+blueprint_discrete_palette <- tribble(
+  ~ name,   ~ r, ~ g, ~ b,
+  "warm_1", 255, 235, 130,
+  "cool_1", 210, 230, 245,
+  "green_1", 220, 230, 130,
+  "warm_2", 250, 190, 120,
+  "cool_2", 155, 185, 220,
+  "green_2", 175, 190, 130,
+  "warm_3", 220, 150, 110
+
+)
+
+
+
+bdp_2 <- blueprint_discrete_palette %>%
+  group_by(name) %>%
+  nest() %>%
+  mutate(
+    hex = data %>%
+      map_chr(
+      ~ rgb(.x$r, .x$g, .x$b, maxColorValue=255)
+      )
+  )
+
+
+rgb_vec <- function(x) {
+
+  names(x) <- c("red", "green", "blue")
+
+  rgb(!!!x, maxColorValue = 255)
+
+}
+
+
+
